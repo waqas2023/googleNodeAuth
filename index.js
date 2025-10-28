a316e7cc5c5c9f2331b935304c311fa01de22490
mwaqas122 <muhammad.waqas@admaxim.com>
2025-10-28 13:05:04 +0500

    Update index.js with latest changes

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const session = require("express-session");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.session());
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }));
 
+
 app.use(passport.initialize());
 
+// app.get("/auth/google",
+//     passport.authenticate("google", { scope: ["profile", "email"] })
+// );
 app.get("/auth/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
+    passport.authenticate("google", { 
+        scope: ["profile", "email"],
+        accessType: "offline",
+        prompt: "consent"
+    })
 );
+
 
 app.get("/auth/google/callback",
     passport.authenticate("google", { session: false }),
